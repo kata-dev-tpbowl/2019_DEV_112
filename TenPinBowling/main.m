8c7f71c154bfef3bb775330b4504a5f61abd628a
Karate Bowling <karatebowling@yahoo.com>
2019-02-24 16:51:34 +0100

    Initial Commit

---
+++ TenPinBowling/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TenPinBowling
+//
+//  Created by Karate Bowling on 24/02/2019.
+//  Copyright © 2019 KarateBowling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+   @autoreleasepool {
+       // insert code here...
+       NSLog(@"Hello, World!");
+   }
+   return 0;
+}
